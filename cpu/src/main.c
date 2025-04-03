ee1a52e7d1be873c4088798007c894fd85465362
deluf <67149013+deluf@users.noreply.github.com>
2025-04-04 00:48:06 +0200

    Added the computation of key metrics

--- cpu/src/main.c
+++ cpu/src/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
+#include <stdlib.h>
 
 #include "bmp_writer.h"
 #include "cam.h"
@@ -11,6 +13,7 @@
 
 #define WIDTH 1024
 #define HEIGHT 1024
+#define ITERATIONS 10
 
 cam_t cam;
 
@@ -27,41 +30,102 @@ vec_t amb_light = {.r = 0.1, .g = 0.1, .b = 0.1};
 
 vec_t pixels[WIDTH*HEIGHT];
 
-int main(){
+double compute_mean(double times[], int count) {
+    double sum = 0.0;
+    for (int i = 0; i < count; i++) {
+        sum += times[i];
+    }
+    return sum / count;
+}
+
+double compute_stddev(double times[], int count, double mean) {
+    double sum = 0.0;
+    for (int i = 0; i < count; i++) {
+        double diff = times[i] - mean;
+        sum += diff * diff;
+    }
+    return sqrt(sum / count);
+}
+
+/* Maybe useful for future reference
+void compute_ci(double mean, double stddev, int count, double* lower, double* upper) {
+    double z = 1.959963984540054;
+    double standard_error = stddev / sqrt(count);
+    *lower = mean - z * standard_error;
+    *upper = mean + z * standard_error;
+}
+*/
+
+int main() {
     cam_init(&cam, &(vec_t){-9, 0, 8}, M_PI/4);
-    //cam.rot.x = -M_PI/6;
     cam.rot.z = -M_PI/3;
     cam.rot.x = -M_PI/6;
+
+    // Load resources once
     spheres = sphere_load("data/spheres.obj", &spheres_len);
     triangles = triangles_load("data/triangles.obj", "data/triangles.mtl", &triangles_len);
     lights = lights_load("data/lights.obj", &lights_len);
-    
-    vec_t screen_points[3];
-    cam_calculate_screen_coords(&cam, screen_points);
-    vec_t ul = screen_points[0];
-    vec_t ur = screen_points[1];
-    vec_t dl = screen_points[2];
-    vec_t inc_x = vec_sub(&ur, &ul);
-    inc_x = vec_div(&inc_x, WIDTH);
-    vec_t inc_y = vec_sub(&dl, &ul);
-    inc_y = vec_div(&inc_y, HEIGHT);
-    for(int y = 0; y < HEIGHT; y++){
-        for(int x = 0; x < WIDTH; x++){
-            vec_t dir = vec_sub(&ul, &cam.pos);
-            vec_t pos_x = vec_mul(&inc_x, x);
-            vec_t pos_y = vec_mul(&inc_y, y);
-            dir = vec_add(&dir, &pos_x);
-            dir = vec_add(&dir, &pos_y);
-            pixels[x+y*WIDTH] = raytrace(cam.pos, dir, 0);
+
+    double times[ITERATIONS];
+
+    for (int i = 0; i < ITERATIONS; i++) {
+        clock_t start = clock();
+
+        vec_t screen_points[3];
+        cam_calculate_screen_coords(&cam, screen_points);
+        vec_t ul = screen_points[0];
+        vec_t ur = screen_points[1];
+        vec_t dl = screen_points[2];
+        vec_t inc_x = vec_sub(&ur, &ul);
+        inc_x = vec_div(&inc_x, WIDTH);
+        vec_t inc_y = vec_sub(&dl, &ul);
+        inc_y = vec_div(&inc_y, HEIGHT);
+
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+                vec_t dir = vec_sub(&ul, &cam.pos);
+                vec_t pos_x = vec_mul(&inc_x, x);
+                vec_t pos_y = vec_mul(&inc_y, y);
+                dir = vec_add(&dir, &pos_x);
+                dir = vec_add(&dir, &pos_y);
+                pixels[x + y * WIDTH] = raytrace(cam.pos, dir, 0);
+            }
         }
+
+        clock_t end = clock();
+        times[i] = (double)(end - start) / (CLOCKS_PER_SEC) * 1000;
+
+        printf("Iteration %d completed in %.3f ms\n", i + 1, times[i]);
     }
 
+    // Only save the last image
     size_t img_len;
     void* img = bmp_write(pixels, WIDTH, HEIGHT, &img_len);
-
-    FILE* fptr = fopen("img.bmp", "wb");
+    FILE* fptr = fopen("render.bmp", "wb");
     fwrite(img, 1, img_len, fptr);
     fclose(fptr);
 
-    printf("END!\n");
+    // Compute metrics
+    double mean = compute_mean(times, ITERATIONS);
+    double stddev = compute_stddev(times, ITERATIONS, mean);
+    
+    //double lower_ci, upper_ci;
+    //compute_ci(mean, stddev, ITERATIONS, &lower_ci, &upper_ci);
+    //printf("Frame time (mean +/- 95%% CI): [%.3f, %.3f] ms\n", lower_ci, upper_ci);
+
+    printf("\n# Scene complexity #\n");
+    printf("Resolution: %d x %d\n", WIDTH, HEIGHT);
+    printf("Number of spheres: %zu\n", spheres_len);
+    printf("Number of triangles: %zu\n", triangles_len);
+    printf("Number of lights: %zu\n", lights_len);
+    // Maybe add more (e.g., # of ray bounces, epsilon, etc.)
+    
+    printf("\n# Metrics #\n");
+    printf("Total execution time of %d frames: %.3f ms\n", ITERATIONS, mean * ITERATIONS);
+    printf("Frame time (mean): %.3f ms\n", mean);
+    printf("Frame time (stddev): %.3f ms^2\n", stddev);
+    printf("Expected FPS: %.3f\n", 1000 / mean);
+    printf("Pixel time (mean): %.3f ms\n", (mean * 1000) / (WIDTH * HEIGHT));
+    
+    return 0;
 }
