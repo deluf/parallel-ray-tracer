dfc9cfcffec9492cf0a38ee6991d12565c672a50
deluf <67149013+deluf@users.noreply.github.com>
2025-05-06 19:11:42 +0200

    Added benchmark script

---
+++ gpu/benchmark.py
@@ -0,0 +1,36 @@
+import os
+import shutil
+from datetime import datetime
+import time
+
+# Define the destination path
+destination_base = "../simulations/"
+
+# Create a folder with the current timestamp
+timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M")
+destination_folder = os.path.join(destination_base, timestamp)
+
+os.makedirs(destination_folder, exist_ok=True)
+print(f"Created folder: {destination_folder}")
+
+print(f"Running make...")
+os.system("make clean")
+os.system("make")
+print("")
+
+for i, j in [(1,1), (2,4), (4,4), (4, 8), (8,8), (8, 16)]:
+
+    # Run the raytracer.exe with the current iteration and redirect outputs
+    output_file = destination_folder + f"/stdout-{i}x{j}.txt"
+    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} > Running ./raytracer {i} {j} ...", end=" ")
+
+    start_time = time.time()
+    os.system(f"./raytracer.exe {i} {j} > {output_file}")
+    elapsed_time = (time.time() - start_time) * 1000.0
+
+    print(f"{elapsed_time:.3f} ms")
+
+# Copy the render.bmp file to the destination folder
+shutil.copy("render.bmp", destination_folder)
+
+print(f"Done!")
